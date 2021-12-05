eeffca8e421937094848811b210ec94ed99871ef
lihuang53 <32201170+lihuang53@users.noreply.github.com>
2021-12-04 22:25:02 -0800

    Sample Part 3

---
+++ Contact.py
@@ -0,0 +1,111 @@
+from ds_messenger import DirectMessage
+from pathlib import Path
+import json, time, os
+
+class DsuFileError(Exception):
+    pass
+
+class DsuProfileError(Exception):
+    pass
+
+
+class Contact:
+    def __init__(self, recipient:str):
+        self.recipient = recipient
+        self.messages = []
+
+
+class Profile:
+    def __init__(self, dsuserver=None, username=None, password=None):
+        self.dsuserver = dsuserver  # REQUIRED
+        self.username = username  # REQUIRED
+        self.password = password  # REQUIRED
+        self.bio = ''  # OPTIONAL
+        self._contacts = []  # OPTIONAL
+
+    def add_contact(self, contact: Contact) -> None:
+        self._contacts.append(contact)
+
+    def del_contact(self, index: int) -> bool:
+        try:
+            del self._contacts[index]
+            return True
+        except IndexError:
+            return False
+
+    def get_contacts(self) -> list:
+        return self._contacts
+
+    def get_contacts_name(self) -> list:
+        name = []
+        for i in range(len(self._contacts)):
+            if self._contacts[i].recipient in name:
+                pass
+            else:
+                name.append(self._contacts[i].recipient)
+
+    def save_profile(self, path: str) -> None:
+        p = Path(path)
+
+        if os.path.exists(p) and p.suffix == '.dsu':
+            try:
+                f = open(p, 'w')
+
+                contacts = {}
+                for i in range(len(self._contacts)):
+                    messages = []
+                    for m in range(len(self._contacts[i].messages)):
+                        messages.append(self._contacts[i].messages[m].__dict__)
+                    contacts[self._contacts[i].recipient] = messages
+
+                p_dict = {
+                    'dsuserver':self.dsuserver,
+                    'username': self.username,
+                    'password': self.password,
+                    'bio': self.bio,
+                    'contacts': contacts
+                }
+                json.dump(p_dict, f)
+
+                f.close()
+            except Exception as ex:
+                raise DsuFileError("An error occurred while attempting to process the DSU file.", ex)
+        else:
+            raise DsuFileError("Invalid DSU file path or type")
+
+    """
+
+    load_profile will populate the current instance of Profile with data stored in a DSU file.
+
+    Example usage: 
+
+    profile = Profile()
+    profile.load_profile('/path/to/file.dsu')
+
+    Raises DsuProfileError, DsuFileError
+
+    """
+
+    def load_profile(self, path: str) -> None:
+        p = Path(path)
+
+        if os.path.exists(p) and p.suffix == '.dsu':
+            try:
+                f = open(p, 'r')
+                obj = json.load(f)
+                print(f'\n{obj}\n')
+                self.username = obj['username']
+                self.password = obj['password']
+                self.dsuserver = obj['dsuserver']
+                self.bio = obj['bio']
+                for recipient, messages in obj['contacts'].items():
+                    c = Contact(recipient)
+                    for dmsg in messages:
+                        c.messages.append(DirectMessage(['recipient'], dmsg['message'],
+                                          dmsg['timestamp'], dmsg['send']))
+                    self._contacts.append(c)
+                f.close()
+            except Exception as ex:
+                raise DsuProfileError(ex)
+        else:
+            raise DsuFileError()
